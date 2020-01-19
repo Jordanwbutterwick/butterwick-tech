af99876b0f4b718a8f8ed090a490554f72754e66
Jordan Butterwick <33608116+Jordanwbutterwick@users.noreply.github.com>
2020-01-19 13:38:39 -0700

    stupid changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
-console.log('HelloWorld');
+console.log("HelloWorld");
+let beginningDate = new Date(2019);
+let currentDate = new Date(2020);
+const yearsOfExperience = 2020 - 2019;
+
+let years = yearsOfExperience;
+
+document.getElementById("experience").innerHTML = years;

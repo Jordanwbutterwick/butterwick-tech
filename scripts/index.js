15a2493d887c53d7ba5058090f41e443bfff365b
Jordan Butterwick <33608116+Jordanwbutterwick@users.noreply.github.com>
2020-05-26 08:27:49 -0600

    Updated website, might take it down later

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,1 @@
-console.log("HelloWorld");
-let beginningDate = new Date(2019);
-let currentDate = new Date(2020);
-const yearsOfExperience = 2020 - 2019;
 
-let years = yearsOfExperience;
-
-document.getElementById("experience").innerHTML = years;

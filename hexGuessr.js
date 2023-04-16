c9b39cb017cd86c7ef76b223aa1852424953bcb7
Jordan Butterwick <jordan@butterwick.tech>
2023-04-16 16:07:04 -0600

    format: a bunch of pages

--- hexGuessr.js
+++ hexGuessr.js
@@ -1,20 +1,36 @@
 
 window.addEventListener("load", () => {3
 
+    const form = document.getElementById("hexguessr");
+    const responder = document.getElementById("responder");
+    const score = document.getElementById("score");
+
+
     function validateGuess(guess) {
-        var reg=/^#?([0-9a-f]{3}){1,2}$/i;
-        console.log(reg.test(guess));
+        const reg = /^#?([0-9a-f]{3}){1,2}$/i;
+        const result = reg.test(guess);
+        console.log(result);
+        return result;
+    }
+
+    function errorResponse(responseText){
+        responder.textContent = responseText;
     }
 
     function makeGuess() {
         const formData = new FormData(form);
         const guess = formData.get("guess");
-        console.log(guess);
+        
+        console.log(`guess: ${guess}`);
 
-        validateGuess(guess);
+        if (!validateGuess(guess)) {
+            errorResponse(`invalid guess: ${guess}, please use a valid hex color code`);
+        }
+
+
     }
     
-    const form = document.getElementById("hexguessr");
+    
     form.addEventListener("submit", (event) => {
         event.preventDefault();
         makeGuess();

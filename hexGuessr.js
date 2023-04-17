0bcb91d737fdc08bce665edfb10a37c69a4e2074
Jordan Butterwick <jordan@butterwick.tech>
2023-04-16 22:12:12 -0600

    feat: fadein

--- hexGuessr.js
+++ hexGuessr.js
@@ -1,20 +1,49 @@
 
-window.addEventListener("load", () => {3
+function generateRandomColors() {
+    const color = Math.random() * 0xfffff * 1000000;
+
+    return '#' + color.toString(16).slice(0, 6);
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+function validateGuess(guess) {
+    const reg = /^#?([0-9a-f]{3}){1,2}$/i;
+    const result = reg.test(guess);
+    console.log(result);
+    return result;
+}
+
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.05;
+    }, 50);
+}
+
+window.addEventListener("load", () => {
+
+    const color = generateRandomColors();
 
     const form = document.getElementById("hexguessr");
     const responder = document.getElementById("responder");
     const score = document.getElementById("score");
 
-
-    function validateGuess(guess) {
-        const reg = /^#?([0-9a-f]{3}){1,2}$/i;
-        const result = reg.test(guess);
-        console.log(result);
-        return result;
-    }
-
-    function errorResponse(responseText){
+    async function errorResponse(responseText){
         responder.textContent = responseText;
+        responder.style.opacity = 1;
+
+        delay(5000).then(() => {
+            fade(responder)
+        })
     }
 
     function makeGuess() {
